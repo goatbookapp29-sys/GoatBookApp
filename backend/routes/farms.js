57b53fc895e0e04bc8d4a4648614c394a2ad84e3
zencodie <zencodie@gmail.com>
2026-03-16 22:16:10 +0530

    Fix TypeError in farms route by using correct auth middleware import

--- backend/routes/farms.js
+++ backend/routes/farms.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getFarmDetails, updateFarmDetails } = require('../controllers/farmController');
-const { protect } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
-router.get('/current', protect, getFarmDetails);
-router.put('/current', protect, updateFarmDetails);
+router.get('/current', auth, getFarmDetails);
+router.put('/current', auth, updateFarmDetails);
 
 module.exports = router;
